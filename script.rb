843a8e61b8440fd9e4e5efabdca84fe8df2ca304
Kelechi125 <kelechi125@yahoo.com>
2024-09-25 14:00:38 -0500

    Added a check at the end of each iteration to see if the array is sorted

--- script.rb
+++ script.rb
@@ -26,31 +26,41 @@ end
 
 def bubble_sort(array)
 
-    array.each_with_index do |num, index|
+  max_num_of_loops = array.length - 1
 
+  max_num_of_loops.times do
+    array.each_with_index do |num, index|
       # Checks if the current index is the array length
       if index == array.length - 1
-
+  
         # Checks if the array is sorted
         if array == array.sort 
           return array
-        elsif array != array.sort
-          continue
         end
-
-        
+          
       elsif index < array.length - 1
-          # Checks if current element is bigger than following element
+        # Checks if current element is bigger than following element
         if array[index] > array[index + 1] 
           # Reassigns the elements to each other's spots
           array[index], array[index + 1] = array[index + 1], array[index]
         end
         # Prints the updated array
         p array
-      end
-      
-    end
   
+        # Checks if the array is sorted
+        if array == array.sort
+          return array
+        end
+      end
+    end
+    
+  end
+
+  return array
 end
 
-bubble_sort([4,3,78,2,0,2])
+#bubble_sort([4,3,78,2,0,2])
+
+#bubble_sort([6, 16, 0, 8, 25])
+
+bubble_sort([100, 18, 99, 21, 1, 45])
